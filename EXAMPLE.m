e59566d60ed4d4ee4a2206c41bdd35160c4fadef
marina.devitis@kuleuven.be <marina.devitis@kuleuven.be>
2018-11-09 17:41:42 +0100

    added option to use sparse guidetubes 0 1

--- EXAMPLE.m
+++ EXAMPLE.m
@@ -1,9 +1,10 @@
 %% input parameters %%
-currDIR = '/mnt/.autofs/storage/gbw-s-neu01_fmri-monkey-03/PROJECT/codeshare/example/surgery/guideTube_Thelma/';
+currDIR = '/data/fmri_monkey_03/PROJECT/Marina/Wells_VVInjection/st04_secondHEM';
+sparseGT = 0;
 
 %specify location 
-imageLocation.guideTubeMask           = [currDIR,'/Thelma180222_guidetubes_sparse.nii'];
-imageLocation.anatomy                 = [currDIR,'/Thelma180222_anat.nii'];
+imageLocation.guideTubeMask           = [currDIR,'/Jango181107_GT_RAS_up03_mask.nii'];
+imageLocation.anatomy                 = [currDIR,'/Jango181107_GT_RAS_up03.nii'];
 
 
 %definition of guide tube threshold and greater then  or less than
@@ -23,20 +24,16 @@ gridpos(1).positionNameLM = 'C';
 gridpos(2).positionNameAP = '7A';
 gridpos(2).positionNameLM = 'C';
 
-gridpos(4).positionNameAP = '7P';
-gridpos(4).positionNameLM = 'C';
+gridpos(3).positionNameAP = '7P';
+gridpos(3).positionNameLM = 'C';
 
-gridpos(3).positionNameAP = 'C';
-gridpos(3).positionNameLM = '7L';
+gridpos(4).positionNameAP = 'C';
+gridpos(4).positionNameLM = '7L';
 
 gridpos(5).positionNameAP = 'C';
 gridpos(5).positionNameLM = '7M';
 
-
-
-
 %% check volume is in correct orientation  %%
-
 
 %order of dimensions [Coronal Transverse Sagittal]
 imageOrient.dimOrder = [1 3 2];
@@ -50,14 +47,14 @@ checkVolOrientation(imageLocation.anatomy,imageOrient);
 
 %% If sparse guide tube (defined only by start and end point) used
 % then fill line between points and dilate line using n_dilate_voxels
-n_dilate_voxels = 5;
-imageLocation = fill_in_guidetube(imageLocation,imageOrient,n_dilate_voxels);
+if(sparseGT==1)
+    n_dilate_voxels = 5;
+    imageLocation = fill_in_guidetube(imageLocation,imageOrient,n_dilate_voxels);
+end
 
 %% calculate mean trajectories and positions and draw grid volume %%
 makeGridImage(imageLocation,imageOrient,guideTube,gridpos);
 
 
 %% get grid position from nifti output of makeGridImage %%
-findGridPos_fromVal(75);
-
-
+findGridPos_fromVal(32);
